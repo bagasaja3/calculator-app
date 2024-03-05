4505cad6691b91c55dd6bd61bdf591b10399a9d4
bagasaja3 <bagasridho@students.amikom.ac.id>
2024-03-05 10:19:17 +0700

    Menambahkan Fungsi penambahan

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+class Calculator 
+ {
+	 static void Main (string[] args)
+	 {
+	
+	 }
+	 
+	 static int Penambahan(int a, int b)
+	 {
+		 return a + b;
+	 }
+	 static int Pengurangan(int a, int b)
+	 {
+		 return a - b;
+	 }
+ }
